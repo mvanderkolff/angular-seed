c4d9b8d51d6306d6fb358a459c13eef8e5f62c85
Michael van der Kolff <mvanderkolff@gmail.com>
2014-08-29 17:23:14 +1000

    MvdK playing around

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,13 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
+angular.module('myApp.controllers', ['myApp.services'])
+  .controller('MyCtrl1', ['$scope', function($scope, userMan) {
+    $scope.user = '';
+    $scope.password = '';
+    $scope.login = function() {
+      userMan.login($scope.user, $scope.password).then(function(u) {console.log(u); alert(u.user_type);});
+    }
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,29 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1').
+  service('userMan', function($q) {
+    
+    this.login = function(user, password) {
+      var deferred = $q.defer();
+      var promise = deferred.promise;
+      if (user == "pc")
+        deferred.resolve({user_type: "pc"});
+      else if (user == "john")
+        deferred.resolve({user_type: "reviewer", specialty: ["ai", "security", "ml"]});
+      else if (user == "alice")
+        deferred.resolve({user_type: "reviewer", specialty: ["bpm", "governance"]});
+      else if (user == "glen")
+        deferred.resolve({user_type: "submitter"});
+      else
+        deferred.reject();
+      return promise;
+    };
+  }).
+  service('paper_man', function() {
+    this.list = function(user) {
+    };
+    this.submit = function(user, paper) {
+    };
+    // These papers will have bid(option, user), review(user, review), discuss(paper, comment) methods, rebut(), select() methods
+  });
